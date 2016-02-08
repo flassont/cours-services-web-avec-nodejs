089215a852c40c2059cf28468ceff2dda3707351
Florian Lassont <florian.lassont@gmail.com>
2016-02-08 08:53:28 +0100

    TP1 - List file in console

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 "use strict";
 
+process.on('uncaughtException', console.error);
 const contactFile = process.env.npm_package_config_contacts;
+
+const fs = require('fs');
+
+fs.readFile(contactFile, (err, data) => {
+    if (err) throw err;
+    const contacts = JSON.parse(data);
+    contacts.forEach(c => console.log(`${c.lastName.toUpperCase()} ${c.firstName}`));
+});

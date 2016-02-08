9674f4679644b02ce0670d3f8205984b439b15da
Florian Lassont <florian.lassont@gmail.com>
2016-02-08 09:37:51 +0100

    TP2 - Command-line interface

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ process.on('uncaughtException', console.error);
 const contactFile = process.env.npm_package_config_contacts;
 
 const fs = require('fs');
+const program = require('commander');
 
-fs.readFile(contactFile, (err, data) => {
-    if (err) throw err;
-    const contacts = JSON.parse(data);
-    contacts.forEach(c => console.log(`${c.lastName.toUpperCase()} ${c.firstName}`));
-});
+program.version('TP2');
+
+program.command('list')
+    .description('List contacts')
+    .action(() => {
+       fs.readFile(contactFile, (err, data) => {
+           if (err) throw err;
+           JSON.parse(data)
+            .forEach(contact => console.log(`${contact.lastName.toUpperCase()} ${contact.firstName}`));
+       });
+    });
+    
+program.command('', {isDefault: true})
+    .action(() => {
+        program.help();
+    });
+    
+program.parse(process.argv);
